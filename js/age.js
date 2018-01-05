7b28002c6e1ecbccc1122a3cbd4c180338a482ab
Amy Churchwell <amy.churchwell@gmail.com>
2018-01-05 10:00:32 -0800

    Add skeleton of specs

--- js/age.js
+++ js/age.js
@@ -14,15 +14,20 @@ export class Age {
   const marsYear = earthYear * 1.88;
   const jupiterYear = earthYear * 11.86;
 
+  function getSecondsByYears(age){
+
+  }
+
   function calcMercury(age) {
-    let age = 2
-    let earthAge = 2 * 365;
-    
+    let age = age;
+    let earthAge = age * 365;
+
+    return earthAge / mercuryYear;
 
   }
 
   function calcVenus(age) {
-
+    
   }
 
   function calcMars(age) {
--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,6 +1,17 @@
 import { Age } from './../js/age.js';
 
 describe('Age', function() {
+  it('should take a person’s age in years and convert it into seconds', function(){
+    let exampleYears = 2;
+    let result = Age.getSecondsByYears(exampleYears);
+    expect(result).toEqual(63113904);
+  });
+
+  it('should return the age of a human in mercury years', funtion(){
+    let humanAge = 2
+    let result = Age.calcMercury(humanAge);
+    expect(result).toBeCloseTo(8.333, 0.1) //TODO: play with this for repeating result
+  })
 
 
 });
