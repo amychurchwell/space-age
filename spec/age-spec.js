a98a082004ab8d18b5d68088bf3624b1a30b09ac
Amy Churchwell <amy.churchwell@gmail.com>
2018-01-05 12:14:49 -0800

    refactor spec file to use beforeEach()

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,32 +1,32 @@
 import { Age } from './../js/age.js';
 
 describe('Age', function() {
+  let age;
+  beforeEach(function(){
+    age = new Age(2);
+  });
+
   it('should take a person’s age in years and convert it into seconds', function(){
-    let age = new Age(2);
     let result = age.getSecondsByYears();
     expect(result).toEqual(63115200);
   });
 
   it('should return the age of a human in mercury years', function(){
-    let age = new Age(2)
     let result = age.calcMercury();
     expect(result).toBeCloseTo(8.333, 0.1) //TODO: play with this for repeating result
   });
 
   it('should return the age of a human in venus years', function(){
-    let age = new Age(2)
     let result = age.calcVenus();
     expect(result).toEqual(3.225806451612903);
   });
 
   it('should return the age of a human in mars years', function(){
-    let age = new Age(2)
     let result = age.calcMars();
     expect(result).toEqual(1.0638297872340428);
   });
 
-  it('should return the age of a human in jupiter years', function(){
-    let age = new Age(2)
+  it('should return the age of a human in jupiter years', function(){    
     let result = age.calcJupiter();
     expect(result).toEqual(0.16863406408094436);
   });
