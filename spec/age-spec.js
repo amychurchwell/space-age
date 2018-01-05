c1d1f19f4a715374495322595fff902615c4a982
Amy Churchwell <amy.churchwell@gmail.com>
2018-01-05 12:00:30 -0800

    Successfully debug karma, by fixing function typos and adding browserify section to karma.conf.js

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -7,25 +7,25 @@ describe('Age', function() {
     expect(result).toEqual(63113904);
   });
 
-  it('should return the age of a human in mercury years', funtion(){
+  it('should return the age of a human in mercury years', function(){
     let humanAge = 2
     let result = Age.calcMercury(humanAge);
     expect(result).toBeCloseTo(8.333, 0.1) //TODO: play with this for repeating result
   });
 
-  it('should return the age of a human in venus years', funtion(){
+  it('should return the age of a human in venus years', function(){
     let humanAge = 2
     let result = Age.calcVenus(humanAge);
     expect(result).toEqual(3.22580645161);
   });
 
-  it('should return the age of a human in mars years', funtion(){
+  it('should return the age of a human in mars years', function(){
     let humanAge = 2
     let result = Age.calcMars(humanAge);
     expect(result).toEqual(1.06382978723);
   });
 
-  it('should return the age of a human in jupiter years', funtion(){
+  it('should return the age of a human in jupiter years', function(){
     let humanAge = 2
     let result = Age.calcJupiter(humanAge);
     expect(result).toEqual(0.16863406408);
